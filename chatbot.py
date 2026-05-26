a9a449baf5597e074d8c60ed9429e04d01abbdf4
Xceptions <agbokenechukwu.k@gmail.com>
2026-05-26 11:19:17 +0100

    integrated vector db into the chatbot.py to retrieve reviews. Fixed some typos in vector.py file

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
@@ -20,5 +21,7 @@ while True:
     print('\n')
     if question == 'q':
         break
-    result = chain.invoke({"reviews": [], "question": question})
+    reviews = retriever.invoke(question) # retrieve relevant documents to our question
+    # result = chain.invoke({"reviews": [], "question": question})
+    result = chain.invoke({"reviews": reviews, "question": question})
     print(result)
--- vector.py
+++ vector.py
@@ -1,6 +1,6 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
-from langchain_core.documents import Documents
+from langchain_core.documents import Document
 
 import os
 import pandas as pd
@@ -27,7 +27,7 @@ if not os.path.exists(db_location):
     for i, row in df.iterrows():
         document = Document(
             page_content = row['Title'] + " " + row['Review'],
-            metadata = {"rating": row["Rating"], "date": row["Date"]}
+            metadata = {"rating": row["Rating"], "date": row["Date"]},
             id=str(i)
         )
         ids.append(str(i))
